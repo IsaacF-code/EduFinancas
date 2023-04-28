5d68fec716934dfdff5acc0038100472f204a40d
Isaac Freires <isaacfreires@gmail.com>
2023-04-28 16:20:43 -0300

    Alterado nome do projeto no title (React App para Sistema de Controle Financeiro); adicionado lib Currency Input

--- edu_financas/src/components/form/FormInput.js
+++ edu_financas/src/components/form/FormInput.js
@@ -1,9 +1,11 @@
 import { useId } from 'react';
 import styles from './FormInput.module.css';
+import CurrencyInput from 'react-currency-input-field';
 
 function Input({ label, type, name, placeholder, value, handleOnChange }){
     const id = useId();
     return (
+       <>
        <div className={styles.form_control}>
         <label htmlFor={id}>{label}:</label>
         <input 
@@ -16,6 +18,8 @@ function Input({ label, type, name, placeholder, value, handleOnChange }){
         />
         
        </div>
+       <CurrencyInput />
+       </>
     )
 }
 
